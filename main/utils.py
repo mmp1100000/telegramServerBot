f3ed9c6b2f6caefe00121ae1dabfadfffde79023
Marco Muñoz Pérez <marcomunozperez@gmail.com>
2020-04-22 16:50:43 +0200

    Fixed process ouput format

--- main/utils.py
+++ main/utils.py
@@ -4,6 +4,7 @@ import subprocess
 def get_system_temperature():
     process = subprocess.Popen(['/opt/vc/bin/vcgencmd', 'measure_temp'],
                                stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True)
     stdout, stderr = process.communicate()
     return stdout
