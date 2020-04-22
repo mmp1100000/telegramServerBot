fff5cea9e0838fd62024b106eaa0eb8392033134
Marco Muñoz Pérez <marcomunozperez@gmail.com>
2020-04-22 17:05:23 +0200

    Added gpiozero stats module

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
-from main.utils import get_system_temperature
+from main.utils import get_system_temperature, get_gpiozero_temperature
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -32,7 +32,7 @@ dispatcher.add_handler(echo_handler)
 
 ###
 def get_temperature(update, context):
-    temp = get_system_temperature()
+    temp = get_gpiozero_temperature()
     print('temp: ' + temp)
     context.bot.send_message(chat_id=update.effective_chat.id, text=temp)
 
--- main/utils.py
+++ main/utils.py
@@ -1,4 +1,10 @@
 import subprocess
+from gpiozero import CPUTemperature
+
+
+def get_gpiozero_temperature():
+    cpu = CPUTemperature()
+    return cpu.temperature
 
 
 def get_system_temperature():
